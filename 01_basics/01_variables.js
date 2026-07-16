dd85f810af43e8cf96dca1fab5f4b7f79b871319
Shailesh-1302 <shaileshananda.panigrahi@gmail.com>
2026-07-16 10:52:49 +0000

    Add JavaScript variables and datatypes examples

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail="subham@gmail.com"
+var accountPassword="i1233"
+accountCity="Jaipur"
+let accountState;
+
+// accountId= 2 // not allowed
+accountEmail="hc@gmail.com"
+accountCity="Begaluru"
+accountPassword="jhi56"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+Because of issues in block and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all code as newer version
+// alert(3+3) // we are using node js, not browser
+
+console.log(3+3) // code readabilty should be higher
+console.log("shailesh")
+
+let name="subham"
+let number=18
+let isLoggedIn=false
+
+//number=>2 to power53
+//bigint
+//string=>""
+//boolean=> true or false
+//null=>standalone value
+//undefined=> 
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof null);//undefined
