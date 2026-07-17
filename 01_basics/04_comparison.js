c199e8902f96c03da50d96ef286ed8f7b5ad2a55
Shailesh-1302 <shaileshananda.panigrahi@gmail.com>
2026-07-17 15:21:36 +0000

    types summary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>=0);
+// console.log(undefined>0);
+// console.log(undefined==0);
+
+
+//these types of conversion must be avoided
+//===
+console.log("2" === 2);// === also checks the datatypes so it will return false
+
---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,45 @@
+//Primitive
+
+// 7 Types:string,number,boolean,null,undefined,symbol,bigint
+
+const score=100
+const scoreValue=100.33
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber=226546425885589557899522n
+
+
+
+
+
+//reference type (non-primitve)
+//array,objects,functions
+const heros=["shaktiman",'naagraj',"doga"]
+let myObj={
+    name:"subham",
+    age:20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");}
+
+
+    console.log(typeof bigNumber);
+    console.log(typeof outsideTemp);
+    console.log(typeof myFunction);
+    console.log(typeof heros);
+
+console.log(typeof id);
+
+
+
+
+      
