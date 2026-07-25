f7e2facbf44775906b66a21140df232765edbf0f
Shailesh-1302 <shaileshananda.panigrahi@gmail.com>
2026-07-25 14:19:44 +0000

    loop34

---
+++ 05_iterations/loop3.js
@@ -0,0 +1,51 @@
+ // for of 
+//  ["",'','']
+//  [{},{},{}]
+
+const arr =[1,2,3,4,5]
+for (const num of arr) {
+       // console.log(num);
+        
+}
+
+const greetings="hello world!"
+for (const greet of greetings){
+    // if( greet==" "){
+    //     console.log("space");
+        
+    //     continue;
+    // }
+   // console.log(`each char is ${greet}`);
+    
+}
+
+//MAPS
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+// map.set('FR',"France")
+
+//console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key,":=",value);
+// }
+
+const myObj ={
+    'game1': "NFS",
+    'game2':"Spiderman"
+}
+//for (const [key,value] of myObj) {
+   // console.log("GAME",key,":",value);  
+//}
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('FR',"France")
+// for (const key in map) {
+   
+//     console.log(key);
+    
+// }
